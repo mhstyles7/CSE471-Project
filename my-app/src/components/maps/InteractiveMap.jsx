b7e353f18c084ef307cd90785f4c9a3a970b48bd
Zarin Tasnim Raisa <152053407+ZarinTasnimRaisa@users.noreply.github.com>
2025-12-04 01:52:23 +0600

    Zarin added heatmap

--- my-app/src/components/maps/InteractiveMap.jsx
+++ my-app/src/components/maps/InteractiveMap.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Heatmap from './Heatmap';
 
 export default function InteractiveMap() {
     return (
-        <div style={{ padding: '20px', height: '400px', backgroundColor: '#e5e7eb', display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: '10px' }}>
-            <h3>Interactive Map Component (Module 4.1 - Zarin)</h3>
+        <div className="w-full">
+            <Heatmap />
         </div>
     );
 }
